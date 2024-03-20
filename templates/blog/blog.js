39036ed18ac0e0b8f08964da41df7cccc95ccf87
justinli-ensemble <69668698+justinli-ensemble@users.noreply.github.com>
2024-03-20 14:17:43 -0700

    complete article template

---
+++ templates/blog/blog.js
@@ -0,0 +1,42 @@
+import { getMetadata } from '../../scripts/aem.js';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function loadLazy(main) {
+  const title = getMetadata('blog-title');
+  const subtitle = getMetadata('subtitle');
+  const quote = getMetadata('quote');
+
+  const blog = main.children[0];
+
+  // Create header format
+  const image = main.querySelector('picture');
+  const imageDiv = document.createElement('div');
+  imageDiv.append(image);
+
+  const titleDiv = document.createElement('div');
+  titleDiv.classList = 'title';
+  titleDiv.textContent = title;
+  const subtitleDiv = document.createElement('div');
+  subtitleDiv.classList = 'subtitle';
+  subtitleDiv.textContent = subtitle;
+  const titleContainer = document.createElement('div');
+  titleContainer.classList = 'title-container';
+  titleContainer.append(subtitleDiv, titleDiv);
+
+  const blogHeading = document.createElement('div');
+  blogHeading.classList = 'blog-heading';
+  blogHeading.append(imageDiv, titleContainer);
+
+  main.append(blogHeading);
+
+  // Create blog post format
+  const quoteDiv = document.createElement('div');
+  quoteDiv.classList = 'quote';
+  quoteDiv.textContent = quote;
+
+  const blogContainer = document.createElement('div');
+  blogContainer.classList = 'blog-container';
+  blogContainer.append(quoteDiv, blog);
+
+  main.append(blogContainer);
+}
